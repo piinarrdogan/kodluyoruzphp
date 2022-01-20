d006cc6372189808cd3b2a93f6b9d59ffa628e21
piinarrdogan <69732378+piinarrdogan@users.noreply.github.com>
2022-01-20 13:44:23 +0300

    Create p.php

---
+++ p.php
@@ -0,0 +1,13 @@
+function triangle()
+{
+
+    for ($i=0; $i<= 10; $i++) {
+        $j=0;
+        while ($j<=$i) {
+            echo "0";
+            $j++;
+        }
+        echo "<br>";
+    }
+}
+triangle();
